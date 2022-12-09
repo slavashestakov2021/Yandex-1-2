e5b13e85dd9317fb1629d44ffc9be2754a8356c6
slavashestakov2005 <slavashestakov2005@gmail.com>
2022-12-10 03:45:14 +0700

    Капучино

--- main.py
+++ main.py
@@ -5,11 +5,43 @@ from PyQt5.QtCore import Qt
 import sqlite3 as db
 
 
+class EditWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.btn.clicked.connect(self.push)
+
+    def push(self):
+        try:
+            v1 = int(self.in1.text()) if self.in1.text() else None
+            v2 = self.in2.text()
+            v3 = self.in3.text()
+            v4 = self.in4.text()
+            v5 = self.in5.text()
+            v6 = int(self.in6.text())
+            v7 = int(self.in7.text())
+            con = db.connect('coffee.sqlite')
+            cur = con.cursor()
+            if v1:
+                cur.execute("""UPDATE coffee SET variety = '{}', roasting = '{}', type = '{}', taste = '{}',
+                price = {}, volume = {} WHERE id = {}""".format(v2, v3, v4, v5, v6, v7, v1))
+            else:
+                cur.execute("""INSERT INTO coffee (variety, roasting, type, taste, price, volume) VALUES
+                ('{}', '{}', '{}', '{}', {}, {})""".format(v2, v3, v4, v5, v6, v7))
+            con.commit()
+            self.close()
+            global ex
+            ex.start()
+        except Exception:
+            self.out.setText('Данные введены не правильно')
+
+
 class Example(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.start()
+        self.editbtn.clicked.connect(self.push)
 
     def start(self):
         con = db.connect('coffee.sqlite')
@@ -32,9 +64,12 @@ class Example(QWidget):
                 self.table.setItem(i, j, item)
         self.table.resizeColumnsToContents()
 
+    def push(self):
+        self.w = EditWindow()
+        self.w.show()
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    ex.show()
-    sys.exit(app.exec())
+
+app = QApplication(sys.argv)
+ex = Example()
+ex.show()
+sys.exit(app.exec())
